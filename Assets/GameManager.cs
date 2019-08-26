c7ef8cef7b16e0ff4525ffeb97cf6374b10573d6
Georgescu Mihai <mihai.georgescu28@yahoo.com>
2019-08-26 15:06:47 +0100

    FirstPush 26/08/19

---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+       
+    public bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameManager gameManager;
+
+
+    public void CompleteLevel()
+
+    {
+        Debug.Log("COMPLETE LEVEL");
+    }
+
+
+
+
+
+    public void EndGame()
+    {
+
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+            //Restart
+        }
+  
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+   
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    
+
+    void OnCollisionEnter(Collision collisionInfo)
+   {
+
+
+        if (collisionInfo.collider.tag == "Obstacles")
+            {
+            Debug.Log("We hit something");
+
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+
+   }
+
+
+
+}
